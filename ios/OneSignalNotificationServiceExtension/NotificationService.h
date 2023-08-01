5eae75156b8cf44d5966be2f5191b992b66a41f3
khuaho <hokhua3@gmail.com>
2023-08-01 14:37:23 +0700

    add onesignal config

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by KhuaHo on 24/07/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
